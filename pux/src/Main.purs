39b4574a6417e28e505a047481bf3b31dd6ce5c1
Miika Hänninen <miika.hanninen@reaktor.com>
2018-03-04 22:25:47 +0200

    Render heading

--- pux/src/Main.purs
+++ pux/src/Main.purs
@@ -1,9 +1,40 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, ($), bind)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Pux (start, EffModel, noEffects, CoreEffects)
+import Pux.Renderer.React (renderToDOM)
+import Pux.DOM.HTML (HTML)
+import Text.Smolder.HTML (div, h1)
+import Text.Smolder.Markup (text)
+
+data State = State { items :: Array Todo }
+
+data Todo = Todo
+    { name :: String
+    , completed :: Boolean
+    }
+
+type Event = Unit
+
+list :: State -> HTML Event
+list (State st) =
+    div do
+       h1 $ text "TODO app"
+
+foldp :: forall fx. Event -> State -> EffModel State Event fx
+foldp _ st = noEffects st
+
+init :: State
+init = State { items: [] }
+
+main :: forall e. Eff (CoreEffects e) Unit
 main = do
-  log "Hello sailor!"
+    app <- start
+        { initialState: init
+        , view: list
+        , foldp
+        , inputs: []
+        }
+    renderToDOM "#root" app.markup app.input
