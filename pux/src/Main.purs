7413c69f7d6b747ca0f3c47580c8949d28f5d844
Miika Hänninen <miika.hanninen@reaktor.com>
2018-03-04 23:27:29 +0200

    Add rest of static rendering

--- pux/src/Main.purs
+++ pux/src/Main.purs
@@ -1,15 +1,21 @@
 module Main where
 
-import Prelude (Unit, ($), bind)
+import Prelude (Unit, ($), bind, discard)
+import Data.Maybe (Maybe(..), fromMaybe, isJust)
+import Data.Traversable (for_)
 import Control.Monad.Eff (Eff)
 
 import Pux (start, EffModel, noEffects, CoreEffects)
 import Pux.Renderer.React (renderToDOM)
 import Pux.DOM.HTML (HTML)
-import Text.Smolder.HTML (div, h1)
-import Text.Smolder.Markup (text)
+import Text.Smolder.HTML as H
+import Text.Smolder.HTML.Attributes as HA
+import Text.Smolder.Markup (text, (!))
 
-data State = State { items :: Array Todo }
+data State = State
+    { items :: Array Todo
+    , notification :: Maybe String
+    }
 
 data Todo = Todo
     { name :: String
@@ -20,14 +26,48 @@ type Event = Unit
 
 list :: State -> HTML Event
 list (State st) =
-    div do
-       h1 $ text "TODO app"
+    H.div do
+       H.h1 $ text "TODO app"
+       notification $ st.notification
+       itemList st.items
+       newItem
+
+notification :: Maybe String -> HTML Event
+notification n =
+    H.div ! HA.className (notificationClassName n) $ text $ fromMaybe "" n
+    where
+          notificationClassName m = if isJust m then "notification active" else "notification passive"
+
+itemList :: Array Todo -> HTML Event
+itemList todos = H.ul $ for_ todos todoItem
+
+todoItem :: Todo -> HTML Event
+todoItem (Todo todo) =
+    H.div do
+        H.span ! HA.className "name" $ text todo.name
+        H.input ! HA.type' "checkbox" ! HA.checked (checkedValue todo.completed)
+        H.a ! HA.className "removeItem" $ text "remove"
+    where
+        checkedValue :: Boolean -> String
+        checkedValue b = if b then "checked" else ""
+
+newItem :: HTML Event
+newItem =
+    H.div ! HA.className "new-item" $ do
+        H.input ! HA.placeholder "new item name"
+        H.button $ text "Add new item"
 
 foldp :: forall fx. Event -> State -> EffModel State Event fx
 foldp _ st = noEffects st
 
 init :: State
-init = State { items: [] }
+init = State
+    { items:
+        [ Todo { name: "learn typescript", completed: false }
+        , Todo { name: "fix handbrake", completed: false }
+        ]
+    , notification: Nothing
+    }
 
 main :: forall e. Eff (CoreEffects e) Unit
 main = do
